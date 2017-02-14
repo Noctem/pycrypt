c9cd177e6dda13effed7dbaa25f330332d9050ca
David Christenson <david@the-david.com>
2017-02-14 15:25:36 -0700

    test manylinux

---
+++ travis/build-wheels.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e -x
+
+# Compile wheels
+for PYBIN in /opt/python/*/bin; do
+    "${PYBIN}/pip" wheel /io/ -w wheelhouse/
+done
+
+# Bundle external shared libraries into the wheels
+for whl in wheelhouse/*.whl; do
+    auditwheel repair "$whl" -w /io/wheelhouse/
+done
+
+# Install packages and test
+for PYBIN in /opt/python/*/bin/; do
+    "${PYBIN}/pip" install pycrypt --no-index -f /io/wheelhouse
+done
