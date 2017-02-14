9e711bf31539214ea080fb3da606de7ad4f906a1
David Christenson <david@the-david.com>
2017-02-14 16:27:22 -0700

    Fix

--- travis/upload-wheels.sh
+++ travis/upload-wheels.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 pip3 install twine
-twine upload -u 'Noctem' -p "$PASS" -r https://pypi.python.org/pypi wheelhouse/*.whl
+twine upload -u 'Noctem' -p "$PASS" --repository-url https://pypi.python.org/pypi wheelhouse/*.whl
 
