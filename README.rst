b2c6fb53488a6377ba142974fbbd0422c22be6f0
David Christenson <david@the-david.com>
2017-02-14 02:30:06 -0700

    Update metadata

---
+++ README.rst
@@ -0,0 +1,6 @@
+Pycrypt
+=======================
+
+A fast C extension for pogo encryption in Python.
+Based on Marcel's excellent pcrypt-c `pcrypt-c
+<https://github.com/laverdet/pcrypt-c>`_.
--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 try:
     from setuptools import setup, Extension
 except ImportError:
@@ -22,6 +24,22 @@ pycrypt = Extension('pycrypt',
                   language='c')
 
 setup (name = 'pycrypt',
-       version = '0.1',
-       description = 'pcrypt-c Python extension.',
+       version = '0.1.0',
+       description = 'Fast pogo encryption.',
+       long_description = 'A fast C extension for Pogo encryption in Python.',
+       url='https://github.com/Noctem/pycrypt',
+       author='David Christenson',
+       author_email='mail@noctem.xyz',
+       classifiers=[
+           'Development Status :: 4 - Beta',
+           'Intended Audience :: Developers',
+           'Operating System :: OS Independent',
+           'Programming Language :: C',
+           'Programming Language :: Python :: 3',
+           'Programming Language :: Python :: 3.4',
+           'Programming Language :: Python :: 3.5',
+           'Programming Language :: Python :: 3.6',
+           'Topic :: Security :: Cryptography',
+       ],
+       keywords='pycrypt pcrypt pogo encryption',
        ext_modules = [pycrypt])
