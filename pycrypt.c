ead03c2e4eaa5a7fa8206b511b631e733d32f69e
David Christenson <mail@noctem.xyz>
2017-04-04 04:34:19 -0600

    C89 compliance, performance enhancement
    Comply with C89 by declaring variable types at the top of functions,
    use smaller integer types, declare integer types manually on old
    versions of Visual Studio.

--- pycrypt.c
+++ pycrypt.c
@@ -17,7 +17,12 @@
 #include <Python.h>
 #include "twofish.h"
 #ifdef _WIN32
+#if (_MSC_VER < 1600)
+typedef unsigned char uint8_t;
+typedef unsigned int uint32_t;
+#else
 #include <stdint.h>
+#endif
 #include <malloc.h>
 #endif
 
@@ -39,30 +44,38 @@ Twofish_Byte enc_key[KEY_SIZE] = { // 1.29.1
 static PyObject *pycrypt(PyObject *self, PyObject *args) {
   const char *input;
   Py_ssize_t len;
-  uint32_t ms;
+  uint32_t ms, state;
+  Twofish_key key;
+  uint8_t xor_byte[BLOCK_SIZE];
+  unsigned char i, block_count;
+  unsigned short offset, output_size;
+#ifdef _WIN32
+  uint8_t * output;
+  PyObject *output_bytes;
+#endif
 
   if (!PyArg_ParseTuple(args, ARG_TYPES, &input, &len, &ms)) {
     return NULL;
   }
 
-  uint32_t state = ms;
+  state = ms;
 
-  Twofish_key key;
   Twofish_prepare_key(enc_key, KEY_SIZE, &key);
 
-  uint8_t xor_byte[BLOCK_SIZE] = { 0, };
-  for (int i = 0; i < BLOCK_SIZE; ++i) {
+  for (i = 0; i < BLOCK_SIZE; ++i) {
     state = (0x41C64E6D * state) + 0x3039;
     xor_byte[i] = (state >> 16) & 0x7FFF;
   }
 
-  int block_count = (len + 256) / 256;
-  int output_size = 4 + (block_count * 256) + 1;
+  block_count = (len + 256) / 256;
+  output_size = 4 + (block_count * 256) + 1;
+
 #ifdef _WIN32
-  uint8_t * output = (uint8_t *)_malloca( output_size );
+  output = (uint8_t *)_malloca( output_size );
 #else
   uint8_t output[output_size];
 #endif
+
   output[0] = (ms >> 24);
   output[1] = (ms >> 16);
   output[2] = (ms >> 8);
@@ -71,8 +84,8 @@ static PyObject *pycrypt(PyObject *self, PyObject *args) {
   memset(output + 4 + len, 0, 256 - len % 256);
   output[output_size - 2] = (uint8_t)(256 - len % 256);
 
-  for (int offset = 0; offset < block_count * 256; offset += BLOCK_SIZE) {
-    for (int i = 0; i < BLOCK_SIZE; i++)
+  for (offset = 0; offset < block_count * 256; offset += BLOCK_SIZE) {
+    for (i = 0; i < BLOCK_SIZE; i++)
       output[4 + offset + i] ^= xor_byte[i];
 
     Twofish_encrypt(&key, output + 4 + offset, output + 4 + offset);
@@ -83,7 +96,6 @@ static PyObject *pycrypt(PyObject *self, PyObject *args) {
   output[output_size - 1] = INTEGRITY_BYTE;
 
 #ifdef _WIN32
-  PyObject *output_bytes;
   output_bytes = PyBytes_FromStringAndSize((char *)output, output_size);
   _freea( output );
   return output_bytes;
@@ -118,12 +130,14 @@ void
 initpycrypt(void)
 #endif
 {
+  PyObject *module;
   Twofish_initialise();
 #if PY_MAJOR_VERSION >= 3
-  PyObject *module = PyModule_Create(&pycryptmodule);
+  module = PyModule_Create(&pycryptmodule);
 #else
-  PyObject *module = Py_InitModule("pycrypt", PycryptMethods);
+  module = Py_InitModule("pycrypt", PycryptMethods);
 #endif
+
   if (module == NULL)
     INITERROR;
 #if PY_MAJOR_VERSION >= 3
